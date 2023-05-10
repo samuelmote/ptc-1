9cd0d5e9ae269c2009092149eabe83a8783eafcd
samuel mote <sammote06@gmail.com>
2023-05-10 14:59:32 +0000

    questions color

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,40 +1,166 @@
 /**
- * @type {{
- *  question: string,
- *  answers: [ string, string ] | [ string, string, string ] | [ string, string, string, string ],
- *  correct: number,
- *  time: number
- * }[] }
- */
+* @type {{
+*  question: string,
+*  answers: [ string, string ] | [ string, string, string ] | [ string, string, string, string ] | [ string, string, string, string, string ],
+*  correct: number,
+*  time: number
+* }[] }
+*/
 export const questions = [
-    {
-        question: "This is the first question.",
-        answers: [
-            "True",
-            "False"
-        ],
-        correct: 0,
-        time: 20
-    },
-    {
-        question: "Who made this question?",
-        answers: [
-            "Ben Thomas",
-            "Mr. Bonner",
-            "Sam Mote",
-            "Camryn Verhey"
-        ],
-        correct: 1,
-        time: 20
-    },
-    {
-        question: "How much time is left in this class?",
-        answers: [
-            "So much",
-            "A respectable amount",
-            "Not enough"
-        ],
-        correct: 2,
-        time: 20
-    }
-]
+   {
+       question: "Is Sam Mote the coolest person to walk the earth?",
+       answers: [
+           "Yes",
+           "No"
+       ],
+       correct: 1,
+       time: 20
+   },
+   {
+       question: "Based on the answer to the last quetion, would you say Sam Mote is very humble.",
+       answers: [
+           "Yes",
+           "No",
+          
+       ],
+       correct: 0,
+       time: 20
+   },
+
+
+   {
+       question: "The answer to this quetion is the answer",
+       answers: [
+           "PIE",
+           "Answer ",
+           "the answer",
+           "The answer"
+       ],
+       correct: 2,
+       time: 20
+   },
+   {
+       question: "If you weighed 99 pounds, and ate 1 pound of fish, would you be...",
+       answers: [
+           "able to breath water",
+           "1% fish",
+           "Sam Mote",
+           "semi-aquatic"
+       ],
+       correct: 1,
+       time: 20
+   },
+   {
+       question: "If a tree falls in the forest with no one to hear it, does it make a sound?",
+       answers: [
+           "No",
+           "Yes",
+           "No one knows",
+       ],
+       correct: 1,
+       time: 20
+   },
+   {
+       question: "If you have 8 pencils, and each one can erase the exact amount it can write, and each one has one 1.5 miles worth of lead, and you write a straight line useing all the pencils, and erase the line all the way back, how far did you walk? and how far are you away from where you were when you started? ",
+       answers: [
+           "100 miles, and 3 miles",
+           "26 miles, and 5 miles",
+           "24 miles, and 0 miles",
+           "40 miles, and 20 miles"
+       ],
+       correct: 2,
+       time: 90
+   },
+   {
+       question: "Who made this question?",
+       answers: [
+           "Ben Thomas",
+           "Mr. Bonner",
+           "Sam Mote",
+           "Camryn Verhey"
+       ],
+       correct: 2,
+       time: 20
+   },
+   {
+       question: "If you have a line of ten dominoes, and they fall over, how many dominoes got hit by another? (assuming each one fell).",
+       answers: [
+           "0",
+           "10",
+           "9",
+           "11"
+       ],
+       correct: 2,
+       time: 30
+
+
+   },
+   {
+       question: "(This sentence it to be taken literally) Bill's heart was broken by his girlfriend, Ted was as big as a mountain, Jen drank as much as a camel, and Bob jumped off a 100ft tall building. who is still alive?",
+       answers: [
+           "they all are",
+           "Bill",
+           "Ted",
+           "Jen"
+          
+       ],
+       correct: 2,
+       time: 35
+   },
+   {
+       question: "If Mr. Bonner is 23, and graduated when he was 18, what year did he graduate?",
+       answers: [
+           "2018",
+           "2023",
+           "2010",
+           "2016"
+       ],
+       correct: 0,
+       time: 30
+   },
+   {
+       question: "If a farmer plants one plant, and the plant doubles each year for 5 years, how many plants will the farmer have?",
+       answers: [
+           "55",
+           "10",
+           "40",
+           "32"
+       ],
+       correct: 3,
+       time: 65
+   },
+   {
+       question: "(find the grammatical error in the following sentences) I ate it? If so, does that not make you the fatter one between the two of us?",
+       answers: [
+           "(,) after so",
+           "(?) after it",
+           "(fatter, should be Fattest)",
+           "there are't any errors"
+       ],
+       correct: 3,
+       time: 30
+   },
+   {
+           question: "If suddenly clouds became soild objects, what happends?",
+           answers: [
+               "Lots of people get hit in the head",
+               "the skys are all blue",
+               "water is wet",
+               "all of the above"
+           ],
+           correct: 3,
+           time: 20
+       },
+       {
+           question: "what came first, physical reality? or spiritual reality?",
+           answers: [
+               "Physical reality",
+               "spiritual reality",
+              
+           ],
+           correct: 1,
+           time: 20
+       } ]
+
+
+
